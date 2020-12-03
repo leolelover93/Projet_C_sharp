ecc889c82f5d66f0ef4586046e80549d3c0e9e7f
MedToken <token.maroc@gmail.com>
2020-12-03 23:12:09 +0100

    update header gestion produit

--- Project-ENSAF/Form1.Designer.cs
+++ Project-ENSAF/Form1.Designer.cs
@@ -37,8 +37,21 @@
             this.checkedLinePanel = new System.Windows.Forms.Panel();
             this.BtnGestionProduits = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.panelSearchBox = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAjouterProduit = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.btnViewALL = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.LogoPanel.SuspendLayout();
+            this.panelHeader.SuspendLayout();
+            this.panelSearchBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -145,11 +158,120 @@
             // 
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(236)))), ((int)(((byte)(228)))));
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(201, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 80, 0, 0);
             this.flowLayoutPanel1.Size = new System.Drawing.Size(790, 499);
             this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.MintCream;
+            this.panelHeader.Controls.Add(this.button5);
+            this.panelHeader.Controls.Add(this.button4);
+            this.panelHeader.Controls.Add(this.btnViewALL);
+            this.panelHeader.Controls.Add(this.panelSearchBox);
+            this.panelHeader.Controls.Add(this.btnAjouterProduit);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(201, 0);
+            this.panelHeader.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(790, 55);
+            this.panelHeader.TabIndex = 2;
+            // 
+            // panelSearchBox
+            // 
+            this.panelSearchBox.Controls.Add(this.pictureBox1);
+            this.panelSearchBox.Controls.Add(this.textBox1);
+            this.panelSearchBox.Location = new System.Drawing.Point(6, 7);
+            this.panelSearchBox.Name = "panelSearchBox";
+            this.panelSearchBox.Size = new System.Drawing.Size(246, 45);
+            this.panelSearchBox.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(198, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 18);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 8F);
+            this.textBox1.Location = new System.Drawing.Point(17, 9);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Cherhcer Produit";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnAjouterProduit
+            // 
+            this.btnAjouterProduit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAjouterProduit.AutoSize = true;
+            this.btnAjouterProduit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(152)))), ((int)(((byte)(207)))));
+            this.btnAjouterProduit.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnAjouterProduit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAjouterProduit.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnAjouterProduit.ForeColor = System.Drawing.Color.White;
+            this.btnAjouterProduit.Location = new System.Drawing.Point(598, 9);
+            this.btnAjouterProduit.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAjouterProduit.Name = "btnAjouterProduit";
+            this.btnAjouterProduit.Size = new System.Drawing.Size(156, 39);
+            this.btnAjouterProduit.TabIndex = 0;
+            this.btnAjouterProduit.Text = "+ Ajouter Prdouit";
+            this.btnAjouterProduit.UseVisualStyleBackColor = false;
+            // 
+            // btnViewALL
+            // 
+            this.btnViewALL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(152)))), ((int)(((byte)(207)))));
+            this.btnViewALL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewALL.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.btnViewALL.ForeColor = System.Drawing.Color.White;
+            this.btnViewALL.Location = new System.Drawing.Point(275, 16);
+            this.btnViewALL.Name = "btnViewALL";
+            this.btnViewALL.Size = new System.Drawing.Size(75, 29);
+            this.btnViewALL.TabIndex = 0;
+            this.btnViewALL.Text = "All";
+            this.btnViewALL.UseVisualStyleBackColor = false;
+            this.btnViewALL.Click += new System.EventHandler(this.filter_style_click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 8F);
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(152)))), ((int)(((byte)(207)))));
+            this.button4.Location = new System.Drawing.Point(356, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 29);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Disponible";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.filter_style_click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 8F);
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(152)))), ((int)(((byte)(207)))));
+            this.button5.Location = new System.Drawing.Point(435, 16);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 29);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Non-Disponible";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.filter_style_click);
             // 
             // Form1
             // 
@@ -157,6 +279,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(991, 499);
+            this.Controls.Add(this.panelHeader);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -166,6 +289,11 @@
             this.panel1.ResumeLayout(false);
             this.LogoPanel.ResumeLayout(false);
             this.LogoPanel.PerformLayout();
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelSearchBox.ResumeLayout(false);
+            this.panelSearchBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -180,6 +308,16 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel LogoPanel;
         private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Button btnAjouterProduit;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Panel panelSearchBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnViewALL;
     }
 }
 
