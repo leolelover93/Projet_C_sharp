d5b84d5c705a638cb8426c1e966aa3fafd5843a4
SaidBelahrach <said.belahrach16@gmail.com>
2020-12-04 11:22:42 +0100

    restore back222222

--- Project-ENSAF/produit_cartUC.Designer.cs
+++ Project-ENSAF/produit_cartUC.Designer.cs
@@ -21,11 +21,7 @@
         }
 
         #region Component Designer generated code
-
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
+         
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(produit_cardUC));
