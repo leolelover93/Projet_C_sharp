1f759226562f65067df4520483bbdb8d323af690
SaidBelahrach <said.belahrach16@gmail.com>
2020-12-09 20:53:08 +0100

    remove dublicated items from form.designer

--- Project-ENSAF/Form1.Designer.cs
+++ Project-ENSAF/Form1.Designer.cs
@@ -39,13 +39,10 @@
             this.panelContainer = new System.Windows.Forms.Panel();
             this.panelGestionVentes = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.textBoxSearchProduitVentes = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel(); 
             this.btnViderPanger = new System.Windows.Forms.Button();
             this.btnAjouterAuPagnier = new System.Windows.Forms.Button();
-            this.labelBasket = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.labelBasket = new System.Windows.Forms.Label(); 
             this.flowLayoutPanelVente = new System.Windows.Forms.FlowLayoutPanel();
             this.listBoxItemProduct = new System.Windows.Forms.Label();
             this.panelGestionProduit = new System.Windows.Forms.Panel();
@@ -58,25 +55,17 @@
             this.tbSearch = new System.Windows.Forms.TextBox();
             this.btnAjouterProduit = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panelGestionVentes = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelGestionVentes = new System.Windows.Forms.Panel();  
             this.buttonSearchPV = new System.Windows.Forms.Button();
-            this.textBoxSearchProduitVentes = new System.Windows.Forms.TextBox();
-            this.btnViderPanger = new System.Windows.Forms.Button();
-            this.btnAjouterAuPagnier = new System.Windows.Forms.Button();
-            this.labelBasket = new System.Windows.Forms.Label();
-            this.pictureBoxBasket = new System.Windows.Forms.PictureBox();
-            this.flowLayoutPanelVente = new System.Windows.Forms.FlowLayoutPanel();
-            this.listBoxItemProduct = new System.Windows.Forms.Label();
+            this.textBoxSearchProduitVentes = new System.Windows.Forms.TextBox();  
+            this.pictureBoxBasket = new System.Windows.Forms.PictureBox();  
             this.panel1.SuspendLayout();
             this.LogoPanel.SuspendLayout();
             this.panelContainer.SuspendLayout();
             this.panelGestionVentes.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBasket)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBasket)).BeginInit(); 
             this.panelGestionProduit.SuspendLayout();
             this.panelHeader.SuspendLayout();
             this.panelSearchBox.SuspendLayout();
@@ -211,8 +200,7 @@
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Controls.Add(this.btnViderPanger);
             this.panel2.Controls.Add(this.btnAjouterAuPagnier);
-            this.panel2.Controls.Add(this.labelBasket);
-            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.labelBasket); 
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
@@ -220,8 +208,7 @@
             this.panel2.TabIndex = 2;
             // 
             // panel3
-            // 
-            this.panel3.Controls.Add(this.button4);
+            //  
             this.panel3.Controls.Add(this.textBoxSearchProduitVentes);
             this.panel3.Location = new System.Drawing.Point(0, 7);
             this.panel3.Name = "panel3";
@@ -240,20 +227,7 @@
             this.textBoxSearchProduitVentes.TabIndex = 2;
             this.textBoxSearchProduitVentes.Text = "Cherhcer Produit";
             this.textBoxSearchProduitVentes.TextChanged += new System.EventHandler(this.textBoxSearchProduitVentes_TextChanged);
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.White;
-            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(200, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(24, 20);
-            this.button4.TabIndex = 4;
-            this.button4.UseVisualStyleBackColor = false;
-            // 
+            //  
             // btnViderPanger
             // 
             this.btnViderPanger.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -307,19 +281,7 @@
             this.labelBasket.TabIndex = 1;
             this.labelBasket.Text = "0";
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(675, 9);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(54, 34);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 0;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
+            // pictureBox2 
             // flowLayoutPanelVente
             // 
             this.flowLayoutPanelVente.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -341,8 +303,7 @@
             this.listBoxItemProduct.Size = new System.Drawing.Size(72, 39);
             this.listBoxItemProduct.TabIndex = 5;
             this.listBoxItemProduct.Visible = false;
-            this.listBoxItemProduct.TextChanged += new System.EventHandler(this.listBoxItemProduct_TextChanged);
-            this.listBoxItemProduct.Resize += new System.EventHandler(this.listBoxItemProduct_Resize);
+            this.listBoxItemProduct.TextChanged += new System.EventHandler(this.listBoxItemProduct_TextChanged); 
             // 
             // panelGestionProduit
             // 
@@ -652,8 +613,7 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.PerformLayout(); 
             this.panelGestionProduit.ResumeLayout(false);
             this.panelHeader.ResumeLayout(false);
             this.panelHeader.PerformLayout();
