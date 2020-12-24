883e42daec5dd55c9ead13ce7834c2bde1204f41
SaidBelahrach <said.belahrach16@gmail.com>
2020-12-24 23:17:16 +0100

    gestion fournisseur added

---
+++ Project-ENSAF/AjouterFournisseurForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_ENSAF
+{
+    public partial class AjouterFournisseurForm : Form
+    {
+        UC_GestionFournisseur theCaller = new UC_GestionFournisseur();
+        Boolean isEditing = false;
+        Fournisseur fournisseursToEdit = new Fournisseur();
+        public AjouterFournisseurForm(UC_GestionFournisseur theCaller)
+        {
+            this.theCaller = theCaller;
+            InitializeComponent();
+        }
+        public AjouterFournisseurForm(Fournisseur fournisseursToEdit, UC_GestionFournisseur theCaller)//used to edit fournisseur
+        {
+            this.fournisseursToEdit = fournisseursToEdit; 
+            this.theCaller = theCaller;
+            initCompo2EditFournisseur(fournisseursToEdit);
+        }
+
+        private void Annuler_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Ajouter_Click(object sender, EventArgs e)
+        {
+            var db = new dbContext();
+            if (!isEditing)
+            {
+                if (tbNom.Text.Length > 0 && tbPrenom.Text.Length > 0 && tbtel.Text.Length > 0 && tbadress.Text.Length > 0)
+                {
+                    try
+                    {
+                        Fournisseur f = new Fournisseur()
+                        {
+                            nomFournisseur = tbNom.Text,
+                            prenomFournisseur = tbPrenom.Text,
+                            adressFournisseur = tbadress.Text,
+                            telFournisseur = tbtel.Text,
+                        }; 
+                        db.Fournisseurs.Add(f);
+                        db.SaveChanges();
+                        MessageBox.Show("Fournisseur ajouté!", "Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close();
+                        theCaller.refrechDataGrid(db.Fournisseurs.ToList());
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("erreur! fournisseur n'est pas ajouter :\n" + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                    MessageBox.Show("Veuillez remplir tous les champs", "Champs vides!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                Fournisseur f2Edit = db.Fournisseurs.Find(fournisseursToEdit.idFournisseur);
+                f2Edit.nomFournisseur = tbNom.Text;
+                f2Edit.prenomFournisseur = tbPrenom.Text;
+                f2Edit.adressFournisseur = tbadress.Text;
+                f2Edit.telFournisseur = tbtel.Text;
+                db.SaveChanges();
+                theCaller.refrechDataGrid(db.Fournisseurs.ToList());
+                this.Close();
+            }
+
+        }
+    }
+}
--- Project-ENSAF/UC_GestionFournisseur.Designer.cs
+++ Project-ENSAF/UC_GestionFournisseur.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Project_ENSAF
 {
-    partial class UC_Gestion_Commades
+    partial class UC_GestionFournisseur
     {
         /// <summary> 
         /// Required designer variable.
@@ -28,15 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.bAjoutCommande = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.nCommandeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dateDemandeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dateArriveSouhaiteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tel = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.produitcommandeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modifier = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Supprimer = new System.Windows.Forms.DataGridViewLinkColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -68,8 +72,9 @@
             this.bAjoutCommande.Name = "bAjoutCommande";
             this.bAjoutCommande.Size = new System.Drawing.Size(145, 28);
             this.bAjoutCommande.TabIndex = 0;
-            this.bAjoutCommande.Text = "Ajouter commande";
+            this.bAjoutCommande.Text = "Ajouter Fournisseur";
             this.bAjoutCommande.UseVisualStyleBackColor = true;
+            this.bAjoutCommande.Click += new System.EventHandler(this.bAjoutCommande_Click);
             // 
             // dataGridView1
             // 
@@ -77,40 +82,52 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeight = 30;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.nCommandeDataGridViewTextBoxColumn,
             this.dateDemandeDataGridViewTextBoxColumn,
             this.dateArriveSouhaiteDataGridViewTextBoxColumn,
-            this.produitcommandeDataGridViewTextBoxColumn});
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 25F);
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.tel,
+            this.produitcommandeDataGridViewTextBoxColumn,
+            this.Modifier,
+            this.Supprimer});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Calibri", 10.75F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
             this.dataGridView1.Location = new System.Drawing.Point(22, 76);
             this.dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(235)))), ((int)(((byte)(255)))));
             this.dataGridView1.Size = new System.Drawing.Size(704, 411);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // nCommandeDataGridViewTextBoxColumn
             // 
             this.nCommandeDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.nCommandeDataGridViewTextBoxColumn.DataPropertyName = "NCommande";
-            this.nCommandeDataGridViewTextBoxColumn.HeaderText = "N commande";
+            this.nCommandeDataGridViewTextBoxColumn.HeaderText = "N fournisseur";
             this.nCommandeDataGridViewTextBoxColumn.Name = "nCommandeDataGridViewTextBoxColumn";
             this.nCommandeDataGridViewTextBoxColumn.ReadOnly = true;
             // 
@@ -118,7 +135,7 @@
             // 
             this.dateDemandeDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.dateDemandeDataGridViewTextBoxColumn.DataPropertyName = "dateDemande";
-            this.dateDemandeDataGridViewTextBoxColumn.HeaderText = "date de demande";
+            this.dateDemandeDataGridViewTextBoxColumn.HeaderText = "Nom";
             this.dateDemandeDataGridViewTextBoxColumn.Name = "dateDemandeDataGridViewTextBoxColumn";
             this.dateDemandeDataGridViewTextBoxColumn.ReadOnly = true;
             // 
@@ -126,27 +143,44 @@
             // 
             this.dateArriveSouhaiteDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.dateArriveSouhaiteDataGridViewTextBoxColumn.DataPropertyName = "dateArriveSouhaite";
-            this.dateArriveSouhaiteDataGridViewTextBoxColumn.HeaderText = "date d\'arrivé souhaité";
+            this.dateArriveSouhaiteDataGridViewTextBoxColumn.HeaderText = "Prénom";
             this.dateArriveSouhaiteDataGridViewTextBoxColumn.Name = "dateArriveSouhaiteDataGridViewTextBoxColumn";
             this.dateArriveSouhaiteDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // tel
+            // 
+            this.tel.HeaderText = "Téléphone";
+            this.tel.Name = "tel";
+            this.tel.ReadOnly = true;
             // 
             // produitcommandeDataGridViewTextBoxColumn
             // 
             this.produitcommandeDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.produitcommandeDataGridViewTextBoxColumn.DataPropertyName = "Produit_commande";
-            this.produitcommandeDataGridViewTextBoxColumn.HeaderText = "Produits commandés";
+            this.produitcommandeDataGridViewTextBoxColumn.HeaderText = "Addresse";
             this.produitcommandeDataGridViewTextBoxColumn.Name = "produitcommandeDataGridViewTextBoxColumn";
             this.produitcommandeDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // UC_Gestion_Commades
+            // Modifier
+            // 
+            this.Modifier.HeaderText = "Modifier";
+            this.Modifier.Name = "Modifier";
+            this.Modifier.ReadOnly = true;
+            // 
+            // Supprimer
+            // 
+            this.Supprimer.HeaderText = "Supprimer";
+            this.Supprimer.Name = "Supprimer";
+            this.Supprimer.ReadOnly = true;
+            // 
+            // UC_GestionFournisseur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "UC_Gestion_Commades";
+            this.Name = "UC_GestionFournisseur";
             this.Size = new System.Drawing.Size(750, 499);
-            this.Load += new System.EventHandler(this.UC_Gestion_Commades_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -154,33 +188,41 @@
 
         }
 
-        private void initCompo(System.Collections.Generic.List<Commande> commandes)
+        private void initCompo(System.Collections.Generic.List<Fournisseur> fournisseurs)
         {
             InitializeComponent();
-            refrechDataGrid(commandes);
-        }
-        public void refrechDataGrid(System.Collections.Generic.List<Commande> commandes)
+            refrechDataGrid(fournisseurs); //first time
+            var db = new dbContext();
+          //  db.Produits
+        } 
+        public void refrechDataGrid(System.Collections.Generic.List<Fournisseur> fournisseurs)
         {
             dataGridView1.Rows.Clear();
             int i = 0;
-            foreach (var item in commandes)
+            foreach (var item in fournisseurs)
             {
-                if (i < commandes.Count - 1) dataGridView1.Rows.Add();
-                dataGridView1.Rows[i].Cells[0].Value = item.NCommande;
-                dataGridView1.Rows[i].Cells[1].Value = item.dateDemande.ToShortDateString();
-                dataGridView1.Rows[i].Cells[2].Value = item.dateArriveSouhaite.ToShortDateString();
-                dataGridView1.Rows[i].Cells[3].Value = "produit1, produit2...";
+                if (i < fournisseurs.Count - 1) dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = item.idFournisseur;
+                dataGridView1.Rows[i].Cells[1].Value = item.nomFournisseur;
+                dataGridView1.Rows[i].Cells[2].Value = item.prenomFournisseur;
+                dataGridView1.Rows[i].Cells[3].Value = item.telFournisseur;
+                dataGridView1.Rows[i].Cells[4].Value = item.adressFournisseur;
+                dataGridView1.Rows[i].Cells[5].Value = "Modifier";
+                dataGridView1.Rows[i].Cells[6].Value = "Supprimer";
                 i++;
             }
         }
-        #endregion
 
+        #endregion
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button bAjoutCommande;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn nCommandeDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dateDemandeDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dateArriveSouhaiteDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tel;
         private System.Windows.Forms.DataGridViewTextBoxColumn produitcommandeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewLinkColumn Modifier;
+        private System.Windows.Forms.DataGridViewLinkColumn Supprimer;
     }
 }
