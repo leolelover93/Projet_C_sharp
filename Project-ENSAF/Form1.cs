a0ed303dcc83a49e7e712b03032ced69d808e728
MedToken <token.maroc@gmail.com>
2021-01-05 22:21:34 +0100

    delete panel(Stock noon dispo )!

--- Project-ENSAF/Form1.cs
+++ Project-ENSAF/Form1.cs
@@ -705,7 +705,6 @@ namespace Project_ENSAF
             {
                 this.pictureBoxBasket.Image = Properties.Resources.cart;
                 produitVentes.Clear(); 
-                flowLayoutPanelVente.Controls.Clear();
                 button1_Click(BtnGestionVentes, e);
                 flowLayoutPanelVente.Refresh();
             }
