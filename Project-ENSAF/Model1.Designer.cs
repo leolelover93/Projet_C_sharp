48e972536d83d430e70f055d241a186294de5c73
SaidBelahrach <said.belahrach16@gmail.com>
2021-02-17 20:51:43 +0100

    added localdb

--- Project-ENSAF/Model1.Designer.cs
+++ Project-ENSAF/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\said.leader\source\repos\projet latest\Project-ENSAF\Project-ENSAF\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\said.leader\source\repos\Projet Csharp\Project-ENSAF\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
